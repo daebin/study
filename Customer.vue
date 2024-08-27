318df4c2e2d0a09a224c318a48ff880c9cde7ae4
daebin <eoqls66@naver.com>
2024-08-27 16:58:17 +0900

    Create Customer.vue

---
+++ Customer.vue
@@ -0,0 +1,19 @@
+<template>
+  <p v-for="Customer in CustomerData" :key="Customer">
+    <input name="customerName" type="radio"> {{ Customer.cus_name }}
+    </p>
+</template>
+
+<script>
+
+export default {
+    name : 'CustomerVue',
+    props : {
+        CustomerData: Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
